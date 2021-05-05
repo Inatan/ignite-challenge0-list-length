c1144355493dc431dd0c3266e2172c6e0b857e1d
inatan <inatan_hertzog@gmail.com>
2021-05-05 09:28:02 -0300

    Finish the challenge

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,17 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call\1" do
+    test "give a list, returns the number of elements" do
+      response = ListLength.call([1, 4, "banana"])
+      expected_response = 3
+      assert response == expected_response
+    end
+
+    test "give a empty list, returns 0" do
+      response = ListLength.call([])
+      expected_response = 0
+      assert response == expected_response
+    end
   end
 end
